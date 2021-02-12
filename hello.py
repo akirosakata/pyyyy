3450b85414460c9252611efd9b26ad1f1abc7423
akirosakata <akirosakata@gmail.com>
2021-02-12 16:09:25 -0500

    commit inicial

---
+++ hello.py
@@ -0,0 +1,22 @@
+# strings
+
+print("hello word")
+print('hello word')
+print(''' hello word''')
+print("""hello word""")
+
+print(type(9))
+
+#list
+[1,2,3,4,5,6,7,8,9]
+["lalo", "augusto", "eli", "aki", "axl"]
+print([True, False,True, False])
+print([])
+
+
+#dictionary par -valor
+print(type({"nombre":"augusto",
+"apellido":"ramos",
+"fec-nac":"16/08/81",
+"edad":"39"
+}))
---
+++ lista.py
@@ -0,0 +1,10 @@
+demo_list = [1,"hello", 1.34, True, [1,2,3]]
+
+
+# list() funcion q permiten definir una lista
+numbres_list = list((1,2,3,4,5))  # una tupla dentro de una lista
+
+print(numbres_list)
+
+print("hola git")
+print("hola ")
